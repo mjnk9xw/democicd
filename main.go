31ddb896498df84077eb3a959b86e9015e4ebe81
i am mn <minhnguyen998vp@gmail.com>
2022-03-21 10:13:38 +0700

    start runner

--- main.go
+++ main.go
@@ -9,5 +9,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/ping2", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong2",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
