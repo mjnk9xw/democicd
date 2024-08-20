5e4bdfac689e600690b85c098aea1bac9349e25c
minh.nguyenvan1 <minhnguyen998vp@gmail.com>
2024-08-20 10:49:19 +0700

    add code

--- models/person_test.go
+++ models/person_test.go
@@ -2,10 +2,13 @@ package models
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPerson(t *testing.T) {
@@ -15,4 +18,11 @@ func TestPerson(t *testing.T) {
 func TestPerson2(t *testing.T) {
 	// panic("error")
 	log.Println("ok")
+}
+
+func TestPerson3(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+	fmt.Println(mock)
 }
